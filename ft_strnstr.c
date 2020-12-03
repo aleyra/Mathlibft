17e91315ca7417f30869f78bd14895e054b5a259
aleyra <75073111+aleyra@users.noreply.github.com>
2020-12-03 13:32:14 +0100

    pour olivier

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: lburnet <lburnet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 10:46:37 by lburnet           #+#    #+#             */
-/*   Updated: 2020/12/02 17:48:49 by lburnet          ###   ########lyon.fr   */
+/*   Updated: 2020/12/03 13:30:45 by lburnet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	if (*needle == 0 || haystack == needle)
+	if (*needle == 0)
 		return ((char *)haystack);
-	while (*haystack != 0 && (ft_strlen(needle) < len--))
+	while (ft_strlen(needle) <= len-- && *haystack != 0) 
 	{
 		if (ft_strncmp(needle, haystack, ft_strlen(needle)) == 0)
 			return ((char *)haystack);
@@ -24,4 +24,22 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 			haystack++;
 	}
 	return (NULL);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+	char 			*str;
+	char 			*to_find;
+	unsigned int	len;
+
+	str = "discnoadscn";
+	to_find = "";
+	len = 3;
+	printf("resultat de strnstr\n");
+	printf("%s\n", strnstr(str, to_find, len));
+	printf("resultat de ft_strnstr\n");
+	printf("%s\n", ft_strnstr(str, to_find, len));
 }
