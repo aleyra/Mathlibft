1cf7e6d84c1770ea87f39368837645f9535982ea
lburnet <lburnet@student.42lyon.fr>
2021-01-19 16:05:36 +0100

    pb de zero repare

--- srcs/base_changing/decimal_to_hexa.c
+++ srcs/base_changing/decimal_to_hexa.c
@@ -6,7 +6,7 @@
 /*   By: lburnet <lburnet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 12:02:30 by lburnet           #+#    #+#             */
-/*   Updated: 2021/01/19 13:48:27 by lburnet          ###   ########lyon.fr   */
+/*   Updated: 2021/01/19 16:05:05 by lburnet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,9 @@
 
 static int		size_in_hexa(unsigned long long nb)
 {
-	unsigned long long	power_base;
 	int					size_in_base;
 
 	size_in_base = 0;
-	power_base = 1;
 	while (nb > 0)
 	{
 		nb /= 16;
@@ -31,11 +29,9 @@ static void		nb_to_hexa(unsigned long long nb, char *str, char *base,
 					int size)
 {
 	unsigned int			new_numeral;
-	unsigned long long		power_base;
 	int						i;
 
 	i = 0;
-	power_base = 1;
 	while (size > 0)
 	{
 		new_numeral = nb % 16;
@@ -52,19 +48,24 @@ char			*decimal_to_hexa(int c, unsigned long long decimal)
 	int		size;
 	char	*str;
 
-	if (c >= 'a' && c <= 'z')
-		base = ft_strdup("0123456789abcdef");
-	else if (c >= 'A' && c <= 'Z')
-		base = ft_strdup("0123456789ABCDEF");
+	(c >= 'a' && c <= 'z') ? base = ft_strdup("0123456789abcdef") : 0;
+	(c >= 'A' && c <= 'Z') ? base = ft_strdup("0123456789ABCDEF") : 0;
+	if (!base)
+		return (NULL);
+	if (decimal == 0)
+	{
+		size = 2;
+		if (!(str = (char *)malloc(size * sizeof(char))))
+			return (NULL);
+		str[0] = '0';
+	}
 	else
 	{
-		base = NULL;
-		return (NULL);
+		size = size_in_hexa(decimal) + 1;
+		if (!(str = (char *)malloc(size * sizeof(char))))
+			return (NULL);
+		nb_to_hexa(decimal, str, base, size - 2);
 	}
-	size = size_in_hexa(decimal) + 1;
-	if (!(str = (char *)malloc(size * sizeof(char))))
-		return (NULL);
-	nb_to_hexa(decimal, str, base, size - 2);
 	str[size - 1] = 0;
 	free(base);
 	return (str);
