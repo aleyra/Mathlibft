01c28fb485543e7c197df20337b9fb8f0f90c350
lburnet <lburnet@student.42lyon.fr>
2021-02-17 10:29:50 +0100

    norme ok

--- srcs/type_changing/ft_ulltoa.c
+++ srcs/type_changing/ft_ulltoa.c
@@ -6,12 +6,11 @@
 /*   By: lucille <lucille@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 12:02:22 by lburnet           #+#    #+#             */
-/*   Updated: 2021/02/17 10:23:48 by lucille          ###   ########lyon.fr   */
+/*   Updated: 2021/02/17 10:27:46 by lucille          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "stdio.h"//
 
 int		ull_len(unsigned long long n, unsigned long long *i)
 {
@@ -26,7 +25,6 @@ int		ull_len(unsigned long long n, unsigned long long *i)
 		*i *= 10;
 		size++;
 	}
-//	printf("size = %d\n", size);//
 	return (size);
 }
 
@@ -37,18 +35,14 @@ char	*ft_ulltoa(unsigned long long n)
 	unsigned long long		i;
 	int						j;
 
-//	printf("n = %llu\n", n);//
 	size = ull_len(n, &i);
-//	printf("size = %d et i = %llu\n", size, i);//
 	if (!(ulltoa = malloc((size + 1) * sizeof(char))))
 		return (NULL);
 	i /= 10;
-//	printf("size = %d et i = %llu\n", size, i);//
 	j = 0;
 	while (i > 1)
 	{
 		ulltoa[j] = '0' + n / i;
-//		printf("ulltoa[%d] = %c et n / i = %llu\n", j, ulltoa[j], n / i);//
 		j++;
 		n %= i;
 		i /= 10;
