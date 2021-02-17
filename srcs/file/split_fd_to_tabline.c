b4822ab3676431e0209af3d1741bfce2f40601c0
lburnet <lburnet@student.42lyon.fr>
2021-02-17 17:24:06 +0100

    add split_fd_to_tabline

--- srcs/file/split_fd_to_tabline.c
+++ srcs/file/split_fd_to_tabline.c
@@ -6,40 +6,29 @@
 /*   By: lucille <lucille@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 14:53:58 by lucille           #+#    #+#             */
-/*   Updated: 2021/02/17 15:25:38 by lucille          ###   ########lyon.fr   */
+/*   Updated: 2021/02/17 17:23:01 by lucille          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int		where_new_line(char *buf)
+static char		**tabline_addline(char **otl, char **line, int n_line)
 {
-	int i;
+	char	**tl;
+	int		i;
 
+	if (!(tl = malloc((n_line + 1) * sizeof(char *))))
+		return (NULL);
 	i = 0;
-	if (!buf)
-		return (-2);
-	while (buf[i])
+	while (i < n_line - 1)
 	{
-		if (buf[i] == '\n')
-			return (i);
+		tl[i] = otl[i];
 		i++;
 	}
-	return (-1);
-}
-
-static int		number_of_line(int fd)
-{
-	int		n_line;
-	int		ret;
-	char	buf[BUFFER_SIZE + 1];
-
-	buf[BUFFER_SIZE] = 0;
-	n_line = 1;
-	while ((ret = read(fd, buf, BUFFER_SIZE)) > 0)
-		if (where_new_line(buf) >= 0)
-			n_line++;
-	return(n_line);
+	tl[i] = *line;
+	tl[i + 1] = NULL;
+	free(otl);
+	return (tl);
 }
 
 char			**split_fd_to_tabline(int fd)
@@ -47,13 +36,17 @@ char			**split_fd_to_tabline(int fd)
 	char	**tl;
 	int		i;
 	int		n_line;
+	char	*line;
 
-	i = 0;
-	n_line = number_of_line(fd);
+	n_line = 1;
 	if (!(tl = malloc((n_line + 1) * sizeof(char *))))
-		return(NULL);
-	while (get_next_line(fd, &(tl[i])) > 0)
-		i++;
-	tl[i + 1] = NULL;
-	return(tl);
+		return (NULL);
+	tl[1] = NULL;
+	tl[0] = NULL;
+	while (get_next_line(fd, &line) > 0)
+	{
+		n_line++;
+		tl = tabline_addline(tl, &line, n_line);
+	}
+	return (tl);
 }
