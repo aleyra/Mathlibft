0211649ef2d977b19d6dab613f28563792cd3a9e
lburnet <lburnet@student.42lyon.fr>
2021-03-04 10:20:46 +0100

    add a security to strcpy

--- srcs/string/ft_strcpy.c
+++ srcs/string/ft_strcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lburnet <lburnet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: lucille <lucille@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/11 09:43:27 by lburnet           #+#    #+#             */
-/*   Updated: 2021/03/03 16:09:12 by lburnet          ###   ########lyon.fr   */
+/*   Updated: 2021/03/04 10:19:27 by lucille          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strcpy(char *dest, const char *src)
 	int	i;
 
 	i = 0;
-	while (src[i])
+	while (src && src[i])
 	{
 		dest[i] = src[i];
 		i++;
