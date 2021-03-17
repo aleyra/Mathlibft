2417d42850b91eafa3358fd03e61accf3de2450c
lburnet <lburnet@student.42lyon.fr>
2021-03-17 11:31:35 +0100

    add fct to find angles

--- includes/maths3d.h
+++ includes/maths3d.h
@@ -6,7 +6,7 @@
 /*   By: lburnet <lburnet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 11:16:19 by lburnet           #+#    #+#             */
-/*   Updated: 2021/03/11 13:28:08 by lburnet          ###   ########lyon.fr   */
+/*   Updated: 2021/03/17 10:31:48 by lburnet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,7 @@ int		in_plane(t_vec3 o, t_vec3 n, t_vec3 p);
 int		on_sphere(t_vec3 o, float d, t_vec3 p);
 int		check_not_aligned(t_vec3 a, t_vec3 b, t_vec3 c);
 t_vec3	normal_of_tr(t_vec3 a, t_vec3 b, t_vec3 c);
+float	angle_one_polaris(t_vec3 v);
+float	angle_two_polaris(t_vec3 v);
 
 #endif
